5566118dec4366abe47941d18b94ba87a6a065e9
NyxoNicolas <68124727+NyxoNicolas@users.noreply.github.com>
2020-08-19 23:55:50 +0200

    Update menu.js

--- .gitignore/menu.js
+++ .gitignore/menu.js
@@ -11,6 +11,7 @@ var prefix = ("n!");
 
 client.on('guildMemberAdd', member => {
     member.guild.channels.cache.get(config.greeting.687208360294219797).send(`Bienvenue ${member} nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+        member.roles.add(config.greeting.687212282266255360)
 })
           
 client.on("message", (message) => {
